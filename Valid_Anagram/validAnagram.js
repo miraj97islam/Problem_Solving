35efa007611272e8dc6643678a2d57282b9e0800
MD MIRAJ ISLAM <52193756+miraj97islam@users.noreply.github.com>
2023-08-14 11:50:14 +0600

    Create  validAnagram.js

---
+++ Valid_Anagram/validAnagram.js
@@ -0,0 +1,24 @@
+string1 = 'mmjrai'
+string2 = 'mirajm'
+
+
+let isAnagram = function(s,t){
+    let sSplited = s.split('')
+    let tCopy = [...t];
+  
+        for(let i=0; i<sSplited.length; i++){
+            if(sSplited.length - i === tCopy.length){          
+               if( tCopy.includes(sSplited[i]) ){
+                tCopy.splice(tCopy.indexOf(sSplited[i]), 1)    //remove matched character
+               }else{
+                return false;
+               } 
+            }else{
+                return false;
+            }
+        }
+
+        return true;
+}
+
+isAnagram(string1,string2);
